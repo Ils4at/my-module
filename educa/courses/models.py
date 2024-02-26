5c0d75ce9cd35e5785cee9b95846f16b54ef3187
Ils4at <sharipov_3000@bk.ru>
2024-02-26 07:55:49 +0300

    page 588

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 #from .fields import OrderField
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -40,3 +42,13 @@ class Course(models.Model):
 #    title = models.CharField(max_length=200)
 #    description = models.TextField()
 #    order = OrderField()
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
